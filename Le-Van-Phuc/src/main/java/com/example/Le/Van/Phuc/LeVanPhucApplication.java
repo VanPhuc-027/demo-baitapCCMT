51e0618adb11838af3e0c97a88ac618579d8424f
VanPhuc-027 <phucvjppromax@gmail.com>
2026-03-16 15:01:59 +0700

    first commit

---
+++ Le-Van-Phuc/src/main/java/com/example/Le/Van/Phuc/LeVanPhucApplication.java
@@ -0,0 +1,13 @@
+package com.example.Le.Van.Phuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeVanPhucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeVanPhucApplication.class, args);
+	}
+
+}
